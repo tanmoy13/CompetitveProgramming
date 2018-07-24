c9342fd88ae14cfa0de45bd273d520d6e9e64e3e
tanmoy13 <tanmoytapos01@gmail.com>
2018-07-25 03:12:21 +0600

    Updated on

--- UVA/1381 - Balancing the Scale.cpp
+++ UVA/1381 - Balancing the Scale.cpp
@@ -57,15 +57,6 @@ using namespace std;
 //typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
 
 
-/*----------------------Graph Moves----------------*/
-//const int fx[]={+1,-1,+0,+0};
-//const int fy[]={+0,+0,+1,-1};
-//const int fx[]={+0,+0,+1,-1,-1,+1,-1,+1};   // Kings Move
-//const int fy[]={-1,+1,+0,+0,+1,+1,-1,-1};  // Kings Move
-//const int fx[]={-2, -2, -1, -1,  1,  1,  2,  2};  // Knights Move
-//const int fy[]={-1,  1, -2,  2, -2,  2, -1,  1}; // Knights Move
-/*------------------------------------------------*/
-
 /*-----------------------Bitmask------------------*/
 int Set(int N,int pos)
 {
