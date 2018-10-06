b92a4195755e65c358767669751d573806ef933c
tanmoy13 <tanmoytapos01@gmail.com>
2018-10-06 13:29:02 +0600

    Updated on 06-10-2018

---
+++ Online-Judge-Solutions/UVA/11159 - Factors and Multiples.cpp
@@ -0,0 +1,182 @@
+/*
+    Author: Tanmoy Datta
+
+    Solution Idea:
+        - Add an bi directional edge between every pair of number (a,b) where b%a=0.
+        - Find Matching of this graph or network.
+        - Maximum matching is our answer.
+*/
+
+#include <bits/stdc++.h>
+// #include <ext/pb_ds/assoc_container.hpp>
+// #include <ext/pb_ds/tree_policy.hpp>
+
+#define pii              pair <int,int>
+#define pll              pair <long long,long long>
+#define sc               scanf
+#define pf               printf
+#define Pi               2*acos(0.0)
+#define ms(a,b)          memset(a, b, sizeof(a))
+#define pb(a)            push_back(a)
+#define MP               make_pair
+#define db               double
+#define ll               long long
+#define EPS              10E-10
+#define ff               first
+#define ss               second
+#define sqr(x)           (x)*(x)
+#define VI               vector <int>
+#define MOD              1000000007
+#define fast_cin         ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define SZ(a)            (int)a.size()
+#define sf(a)            scanf("%d",&a)
+#define sfl(a)           scanf("%lld",&a)
+#define sff(a,b)         scanf("%d %d",&a,&b)
+#define sffl(a,b)        scanf("%lld %lld",&a,&b)
+#define sfff(a,b,c)      scanf("%d %d %d",&a,&b,&c)
+#define sfffl(a,b,c)     scanf("%lld %lld %lld",&a,&b,&c)
+#define stlloop(v)       for(__typeof(v.begin()) it=v.begin();it!=v.end();it++)
+#define UNIQUE(v)        (v).erase(unique((v).begin(),(v).end()),(v).end())
+#define POPCOUNT         __builtin_popcountll
+#define RIGHTMOST        __builtin_ctzll
+#define LEFTMOST(x)      (63-__builtin_clzll((x)))
+#define NUMDIGIT(x,y)    (((vlong)(log10((x))/log10((y))))+1)
+#define NORM(x)          if(x>=mod) x-=mod;if(x<0) x+=mod;
+#define ODD(x)           (((x)&1)==0?(0):(1))
+#define loop(i,n)        for(int i=0;i<n;i++)
+#define loop1(i,n)       for(int i=1;i<=n;i++)
+#define REP(i,a,b)       for(int i=a;i<b;i++)
+#define RREP(i,a,b)      for(int i=a;i>=b;i--)
+#define TEST_CASE(t)     for(int z=1;z<=t;z++)
+#define PRINT_CASE       printf("Case %d: ",z)
+#define LINE_PRINT_CASE  printf("Case %d:\n",z)
+#define CASE_PRINT       cout<<"Case "<<z<<": "
+#define all(a)           a.begin(),a.end()
+#define intlim           2147483648
+#define infinity         (1<<28)
+#define ull              unsigned long long
+#define gcd(a, b)        __gcd(a, b)
+#define lcm(a, b)        ((a)*((b)/gcd(a,b)))
+#define D(x)        	 cerr << __LINE__ << ": " << #x << " = " << (x) << '\n'
+#define DD(x,y)          cerr << __LINE__ << ": " << #x << " = " << x << "   " << #y << " = " << y << '\n'
+#define DDD(x,y,z)       cerr << __LINE__ << ": " << #x << " = " << x << "   " << #y << " = " << y << "   " << #z << " = " << z << '\n'
+#define DBG              cerr << __LINE__ << ": Hi" << '\n'
+
+using namespace std;
+
+//using namespace __gnu_pbds;
+//typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
+
+
+/*----------------------Graph Moves----------------*/
+//const int fx[]={+1,-1,+0,+0};
+//const int fy[]={+0,+0,+1,-1};
+//const int fx[]={+0,+0,+1,-1,-1,+1,-1,+1};   // Kings Move
+//const int fy[]={-1,+1,+0,+0,+1,+1,-1,-1};  // Kings Move
+//const int fx[]={-2, -2, -1, -1,  1,  1,  2,  2};  // Knights Move
+//const int fy[]={-1,  1, -2,  2, -2,  2, -1,  1}; // Knights Move
+/*------------------------------------------------*/
+
+/*-----------------------Bitmask------------------*/
+//int Set(int N,int pos){return N=N | (1<<pos);}
+//int reset(int N,int pos){return N= N & ~(1<<pos);}
+//bool check(int N,int pos){return (bool)(N & (1<<pos));}
+/*------------------------------------------------*/
+
+#define mx 205
+
+vector<int>g[mx];
+int n,m;
+bool vis[mx];
+int match[mx];
+
+bool find_match(int u)
+{
+    vis[u]=1;
+    for(int i=0;i<SZ(g[u]);i++)
+    {
+        int v=g[u][i];
+        if(vis[v]) continue;
+        vis[v]=1;
+        if(match[v]==-1 || find_match(match[v]))
+        {
+            match[v]=u;
+            match[u]=v;
+            return true;
+        }
+    }
+    return false;
+}
+
+int main()
+{
+//#ifndef ONLINE_JUDGE
+//    freopen("in.txt","r",stdin);
+////	  freopen("out.txt","w",stdout);
+//#endif
+
+    int t;
+    sf(t);
+    TEST_CASE(t)
+    {
+        sf(n);
+        vector<ll>v1,v2;
+        for(int i=0;i<n;i++)
+        {
+            ll a;
+            sfl(a);
+            v1.pb(a);
+        }
+        sf(m);
+        for(int i=0;i<m;i++)
+        {
+            ll a;
+            sfl(a);
+            v2.pb(a);
+        }
+
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                if(v1[i]==v2[j])
+                {
+                    g[i].pb(n+j);
+                    g[n+j].pb(i);
+                    continue;
+                }
+                if(v1[i]==0) continue;
+                ll mod = v2[j]%v1[i];
+                mod = (mod+v1[i])%v1[i];
+                if(mod==0)
+                {
+                    g[i].pb(n+j);
+                    g[n+j].pb(i);
+                }
+            }
+        }
+
+        ms(match,-1);
+        int cnt=0;
+
+        for(int i=0;i<n;i++)
+        {
+            ms(vis,0);
+            if(find_match(i))
+                cnt++;
+        }
+
+        PRINT_CASE;
+
+        printf("%d\n",cnt);
+
+        for(int i=0;i<mx;i++)
+            g[i].clear();
+
+
+    }
+
+
+
+    return 0;
+}
