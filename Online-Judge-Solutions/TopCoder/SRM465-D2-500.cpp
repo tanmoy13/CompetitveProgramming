611e45d277a8fdcc7a2a8bf7f628ae0e6b3284c6
tanmoy13 <tanmoytapos01@gmail.com>
2018-10-08 03:09:33 +0600

    Updated on 08-10-2018

---
+++ Online-Judge-Solutions/TopCoder/SRM465-D2-500.cpp
@@ -0,0 +1,220 @@
+#include <bits/stdc++.h>
+// #include <ext/pb_ds/assoc_container.hpp>
+// #include <ext/pb_ds/tree_policy.hpp>
+
+#define pii              pair <int,int>
+#define pll              pair <long long,long long>
+#define sc               scanf
+#define pf               printf
+#define Pi               2*acos(0.0)
+#define ms(a,b)          memset(a, b, sizeof(a))
+#define pb(a)            push_back(a)
+#define MP               make_pair
+#define db               double
+#define ll               long long
+#define EPS              10E-10
+#define ff               first
+#define ss               second
+#define sqr(x)           (x)*(x)
+#define VI               vector <int>
+#define MOD              1000000007
+#define fast_cin         ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define SZ(a)            (int)a.size()
+#define sf(a)            scanf("%d",&a)
+#define sfl(a)           scanf("%lld",&a)
+#define sff(a,b)         scanf("%d %d",&a,&b)
+#define sffl(a,b)        scanf("%lld %lld",&a,&b)
+#define sfff(a,b,c)      scanf("%d %d %d",&a,&b,&c)
+#define sfffl(a,b,c)     scanf("%lld %lld %lld",&a,&b,&c)
+#define stlloop(v)       for(__typeof(v.begin()) it=v.begin();it!=v.end();it++)
+#define UNIQUE(v)        (v).erase(unique((v).begin(),(v).end()),(v).end())
+#define POPCOUNT         __builtin_popcountll
+#define RIGHTMOST        __builtin_ctzll
+#define LEFTMOST(x)      (63-__builtin_clzll((x)))
+#define NUMDIGIT(x,y)    (((vlong)(log10((x))/log10((y))))+1)
+#define NORM(x)          if(x>=mod) x-=mod;if(x<0) x+=mod;
+#define ODD(x)           (((x)&1)==0?(0):(1))
+#define loop(i,n)        for(int i=0;i<n;i++)
+#define loop1(i,n)       for(int i=1;i<=n;i++)
+#define REP(i,a,b)       for(int i=a;i<b;i++)
+#define RREP(i,a,b)      for(int i=a;i>=b;i--)
+#define TEST_CASE(t)     for(int z=1;z<=t;z++)
+#define PRINT_CASE       printf("Case %d: ",z)
+#define LINE_PRINT_CASE  printf("Case %d:\n",z)
+#define CASE_PRINT       cout<<"Case "<<z<<": "
+#define all(a)           a.begin(),a.end()
+#define intlim           2147483648
+#define infinity         (1<<28)
+#define ull              unsigned long long
+#define gcd(a, b)        __gcd(a, b)
+#define lcm(a, b)        ((a)*((b)/gcd(a,b)))
+#define D(x)        	 cerr << __LINE__ << ": " << #x << " = " << (x) << '\n'
+#define DD(x,y)          cerr << __LINE__ << ": " << #x << " = " << x << "   " << #y << " = " << y << '\n'
+#define DDD(x,y,z)       cerr << __LINE__ << ": " << #x << " = " << x << "   " << #y << " = " << y << "   " << #z << " = " << z << '\n'
+#define DBG              cerr << __LINE__ << ": Hi" << '\n'
+
+using namespace std;
+
+//using namespace __gnu_pbds;
+//typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
+
+
+/*----------------------Graph Moves----------------*/
+//const int fx[]={+1,-1,+0,+0};
+//const int fy[]={+0,+0,+1,-1};
+//const int fx[]={+0,+0,+1,-1,-1,+1,-1,+1};   // Kings Move
+//const int fy[]={-1,+1,+0,+0,+1,+1,-1,-1};  // Kings Move
+//const int fx[]={-2, -2, -1, -1,  1,  1,  2,  2};  // Knights Move
+//const int fy[]={-1,  1, -2,  2, -2,  2, -1,  1}; // Knights Move
+/*------------------------------------------------*/
+
+/*-----------------------Bitmask------------------*/
+//int Set(int N,int pos){return N=N | (1<<pos);}
+//int reset(int N,int pos){return N= N & ~(1<<pos);}
+//bool check(int N,int pos){return (bool)(N & (1<<pos));}
+/*------------------------------------------------*/
+
+class TurretPlacement
+{
+public:
+	long long count(vector <int> x, vector <int> y)
+	{
+        int n=SZ(x);
+        ll ret=0;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=i+1;j<n;j++)
+            {
+                ll dist=2.0*hypot(x[i]-x[j],y[i]-y[j]);
+                dist-=1;
+                ret+=(dist*(dist+1))/2;
+
+            }
+        }
+        return ret;
+	}
+};
+
+
+template<typename T> void print( T a ) {
+    cerr << a;
+}
+
+void print( long long a ) {
+    cerr << a << "L";
+}
+
+void print( string a ) {
+    cerr << '"' << a << '"';
+}
+
+template<typename T> void print( vector<T> a ) {
+    cerr << "{";
+    for ( int i = 0 ; i != a.size() ; i++ ) {
+        if ( i != 0 ) cerr << ", ";
+        print( a[i] );
+    }
+    cerr << "}" << endl;
+}
+
+template<typename T> void assert_eq( int n, T have, T need ) {
+    if ( have == need ) {
+        cerr << "Case " << n << " passed." << endl;
+    } else {
+        cerr << "Case " << n << " failed: expected ";
+        print( need );
+        cerr << " received ";
+        print( have );
+        cerr << "." << endl;
+    }
+}
+
+template<typename T> void assert_eq( int n, vector<T> have, vector<T> need ) {
+    if( have.size() != need.size() ) {
+        cerr << "Case " << n << " failed: returned " << have.size() << " elements; expected " << need.size() << " elements.";
+        print( have );
+        print( need );
+        return;
+    }
+    for( int i= 0; i < have.size(); i++ ) {
+        if( have[i] != need[i] ) {
+            cerr << "Case " << n << " failed. Expected and returned array differ in position " << i << ".";
+            print( have );
+            print( need );
+            return;
+        }
+    }
+    cerr << "Case " << n << " passed." << endl;
+}
+void assert_eq( int n, string have, string need ) {
+    if ( have == need ) {
+        cerr << "Case " << n << " passed." << endl;
+    } else {
+        cerr << "Case " << n << " failed: expected ";
+        print( need );
+        cerr << " received ";
+        print( have );
+        cerr << "." << endl;
+    }
+}
+
+int main( int argc, char* argv[] )
+{
+
+	TurretPlacement objectTurretPlacement;
+
+	//test case0
+	vector <int> param00;
+	param00.push_back(0);
+	param00.push_back(2);
+	vector <int> param01;
+	param01.push_back(0);
+	param01.push_back(2);
+	long long ret0 = objectTurretPlacement.count(param00,param01);
+	long long need0 = 10;
+	assert_eq(0,ret0,need0);
+
+	//test case1
+	vector <int> param10;
+	param10.push_back(0);
+	param10.push_back(1);
+	param10.push_back(2);
+	vector <int> param11;
+	param11.push_back(0);
+	param11.push_back(1);
+	param11.push_back(0);
+	long long ret1 = objectTurretPlacement.count(param10,param11);
+	long long need1 = 8;
+	assert_eq(1,ret1,need1);
+
+	//test case2
+	vector <int> param20;
+	param20.push_back(1);
+	param20.push_back(2);
+	param20.push_back(3);
+	param20.push_back(0);
+	vector <int> param21;
+	param21.push_back(-1);
+	param21.push_back(-5);
+	param21.push_back(-7);
+	param21.push_back(100);
+	long long ret2 = objectTurretPlacement.count(param20,param21);
+	long long need2 = 65137;
+	assert_eq(2,ret2,need2);
+
+	//test case3
+	vector <int> param30;
+	param30.push_back(9998);
+	param30.push_back(-10000);
+	param30.push_back(10000);
+	param30.push_back(0);
+	vector <int> param31;
+	param31.push_back(9998);
+	param31.push_back(10000);
+	param31.push_back(10000);
+	param31.push_back(0);
+	long long ret3 = objectTurretPlacement.count(param30,param31);
+	long long need3 = 2799564895;
+	assert_eq(3,ret3,need3);
+
+}
